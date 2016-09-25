24a04fe89e7ca382a2c4fc0c460d1925328c7bc3
Fernando Seguim <fernando.seguim@outlook.com>
2016-09-25 15:17:27 -0300

    user - doing

---
+++ QoalaWebApplication/Website/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class User
+    {
+        public enum ePerfil { Publico = 1, Administrador = 2, Editor = 3 } 
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+        
+        public int Perfil { get; set; }
+
+        public int newPassword { get; set; }
+        
+
+    }
+}
