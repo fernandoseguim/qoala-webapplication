a983ea12b0f8b71394c7158dcf7a972adf63553f
Lucas Gomide <lucaslg200@gmail.com>
2016-09-26 13:32:22 -0300

    Cleanup

--- QoalaWebApplication/Website/Models/User.cs
+++ QoalaWebApplication/Website/Models/User.cs
@@ -1,22 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Website.Models
+﻿namespace Website.Models
 {
     public class User
     {
-        public enum ePerfil { Publico = 1, Administrador = 2, Editor = 3 } 
+        public enum Profile { Publico = 1, Administrador = 2, Editor = 3 } 
 
-        public string Nome { get; set; }
+        public string Name { get; set; }
 
         public string Email { get; set; }
-        
-        public int Perfil { get; set; }
-
-        public int newPassword { get; set; }
-        
-
     }
 }
