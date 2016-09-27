1174f7680ff9c6a463be3e41d9e887199a93585c
Lucas Gomide <lucaslg200@gmail.com>
2016-09-26 23:56:25 -0300

    Add Model Post

---
+++ QoalaWebApplication/Website/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Website.Models
+{
+    public class Post
+    {
+        public int IdPost { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string PublishedAt { get; set; }
+        public int IdUser { get; set; }
+    }
+}
---
+++ QoalaWebApplication/Website/Models/ViewModels/BlogViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Website.Models.ViewModels
+{
+    public class BlogViewModel
+    {
+        public List<Post> Posts { get; set; }
+        public bool HasMorePages { get; set; }
+        public int TotalNumberPages { get; set; }
+    }
+}
