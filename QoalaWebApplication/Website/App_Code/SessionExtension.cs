aa7c2a0e01a1d9de91e6279725ccce0c9931ede9
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2016-09-27 22:48:15 -0300

    Extension for get CurrentUser() from Session

---
+++ QoalaWebApplication/Website/App_Code/SessionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+using Website.Models.ViewModels;
+
+namespace System.Web.WebPages
+{
+    public static class SessionExtensions
+    {
+        public static UserViewModel CurrentUser(this HttpSessionStateBase obj)
+        {
+            return (UserViewModel)obj["CurrentUser"];
+        }
+    }
+}
