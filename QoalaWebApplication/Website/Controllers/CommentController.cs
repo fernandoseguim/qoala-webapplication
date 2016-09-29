6b5e2984b605640d1557f22b18b5229ee2957dcc
Lucas Gomide <lucaslg200@gmail.com>
2016-09-29 15:05:22 -0300

    Creating Route to list all comments from post by author

--- QoalaWebApplication/Website/Controllers/CommentController.cs
+++ QoalaWebApplication/Website/Controllers/CommentController.cs
@@ -33,5 +33,33 @@ namespace Website.Controllers
             data = new { success = true };
             return new JsonResult() { Data = data };
         }
+
+        [HttpGet]
+        [AuthorizationRequest]
+        public ActionResult Index()
+        {
+            var user = (UserViewModel) Session["CurrentUser"];
+            WSRequest request = new WSRequest("users/" + user.IdUser + "/posts/comments");
+            request.AddAuthorization(Session["token"].ToString());
+            var response = request.Get();
+            if (response.Code != 200)
+                return RedirectToAction("Index", "Home", new { message = "Não foi possível buscar esse post" });
+            
+            var body = response.Body;
+            List<CommentViewModel> model = new List<CommentViewModel>();
+            foreach (var comment in body.GetValue("comments"))
+            {
+                model.Add(
+                    new CommentViewModel
+                    {
+                        IdPost = (int)comment["id_post"],
+                        Content = comment["content"].ToString(),
+                        IdUser = (int)comment["id_user"]
+                    }
+                );
+            }
+
+            return View(model);
+        }
     }
 }
--- QoalaWebApplication/Website/Models/ViewModels/CommentViewModel.cs
+++ QoalaWebApplication/Website/Models/ViewModels/CommentViewModel.cs
@@ -4,11 +4,13 @@ namespace Website.Models.ViewModels
 {
     public class CommentViewModel
     {
+        public int IdComment { get; set; }
         public int IdPost { get; set; }
         [Required]
         [Display(Name = "Conteúdo")]
         [StringLength(100, ErrorMessage = "O tamanho do nome deve respeitar o mínimo de {2} e máximo {1}", MinimumLength = 6)]
         public string Content { get; set; }
+        public string ApprovedAt { get; set; }
         public int IdUser { get; set; }
     }
 }
