212ba22023ef53c148d9c6c72ca02da912937fb7
Lucas Gomide <lucaslg200@gmail.com>
2016-10-06 19:10:14 -0300

    Custom Show Profile

--- QoalaWebApplication/Website/App_Code/SessionExtension.cs
+++ QoalaWebApplication/Website/App_Code/SessionExtension.cs
@@ -8,5 +8,19 @@ namespace System.Web.WebPages
         {
             return (UserViewModel)obj["CurrentUser"];
         }
+
+        public static string Token(this HttpSessionStateBase obj)
+        {
+            var token = obj["token"];
+            if (token == null)
+                return null;
+            return token.ToString();
+        }
+
+        public static bool IsLogged(this HttpSessionStateBase obj)
+        {
+            var token = obj["token"];
+            return token != null;
+        }
     }
 }
