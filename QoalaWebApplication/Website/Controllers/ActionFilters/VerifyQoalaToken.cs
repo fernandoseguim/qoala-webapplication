fb538dc510e2b76578e0e537cd2fe58003498bd9
Fernando Seguim <fernando.seguim@outlook.com>
2016-10-11 20:54:59 -0300

    Fix crash post + Adjusts marckup layout

--- QoalaWebApplication/Website/Controllers/ActionFilters/VerifyQoalaToken.cs
+++ QoalaWebApplication/Website/Controllers/ActionFilters/VerifyQoalaToken.cs
@@ -44,6 +44,41 @@ namespace Website.Controllers.ActionFilters
 
                     HttpContext.Current.Session["CurrentUser"] = user;
                 }
+            } else
+            {
+                if (HttpContext.Current.Request.Cookies["qoala_token"] != null)
+                {
+                    token = HttpContext.Current.Request.Cookies["qoala_token"].Value;
+                } else
+                {
+                    token = HttpContext.Current.Session["token"].ToString();
+                }
+
+                WSRequest request = new WSRequest("accounts/me");
+
+                request.AddAuthorization(token.ToString());
+
+                var response = request.Get();
+                if (response.Code != 200)
+                {
+                    HttpContext.Current.Session["token"] = null;
+                    HttpContext.Current.Session["CurrentUser"] = null;
+                    var cookie = new HttpCookie("qoala_token");
+                    cookie.Expires = DateTime.Now.AddDays(-1d);
+                    HttpContext.Current.Response.Cookies.Add(cookie);
+                    return;
+                }
+                var body = response.Body;
+
+                Models.ViewModels.UserViewModel user = new Models.ViewModels.UserViewModel
+                {
+                    IdUser = (int)body.GetValue("id_user"),
+                    Email = body.GetValue("email").ToString(),
+                    Name = body.GetValue("name").ToString(),
+                    Permission = (int)body.GetValue("permission")
+                };
+
+                HttpContext.Current.Session["CurrentUser"] = user;
             }
         }
     }
