243251b6023e3283b676f169eb295addc5f5e5af
Lucas Gomide <lucaslg200@gmail.com>
2016-10-20 20:08:12 -0200

    Get Plans

--- QoalaWebApplication/Website/Controllers/HomeController.cs
+++ QoalaWebApplication/Website/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using System.Web.Hosting;
+﻿using System.Collections.Generic;
+using System.Web.Hosting;
 using System.Web.Mvc;
+using Website.Models.API;
+using Website.Models.ViewModels;
+using Website.Models.ViewModels.Sponsor;
 
 namespace Website.Controllers
 {
@@ -9,8 +13,36 @@ namespace Website.Controllers
         {
             return View();
         }
+
         public ActionResult Index()
         {
+            var user = (UserViewModel)Session["CurrentUser"];
+            if(user != null && user.Permission == 4)
+            {
+                WSRequest request = new WSRequest("plans/");
+                var response = request.Get();
+                if (response.Code != 200)
+                {
+                    return RedirectToAction("Index", "Home", new { message = "Não foi possível buscar esse plano" });
+                }
+                var body = response.Body;
+                List<PlanViewModel> list = new List<PlanViewModel>();
+
+                foreach (var item in body.GetValue("plans"))
+                {
+                    list.Add(
+                        new PlanViewModel
+                        {
+                            IdPlan = (int)item["id_plan"],
+                            Left = (int)item["left"],
+                            Name = item["name"].ToString(),
+                            Price_cents = (int)item["price_cents"],
+                            Rewards = item["rewards"].ToString(),
+                        }
+                    );
+                }
+                ViewBag.plans = list;
+            }
             return View();
         }
 
